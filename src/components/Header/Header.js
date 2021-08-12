e42a4e944fa862818b6c0dcfcae4df3123caa77c
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-12 20:52:08 +0200

    finished the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import React from "react";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillFacebook,
+} from "react-icons/ai";
 import { DiCssdeck } from "react-icons/di";
 
 import {
@@ -21,6 +25,34 @@ const Header = () => (
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/BillBenon">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://twitter.com/benon_bill">
+        <AiFillTwitterCircle size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.facebook.com/bill.benon">
+        <AiFillFacebook size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 

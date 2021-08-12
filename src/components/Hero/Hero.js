a86e542bdfa5cc5de60e9dae17821c6e43b85380
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-12 22:44:44 +0200

    hero component finished

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello! My name is <br />
+        Dushime Bill Benon
+      </SectionTitle>
+      <SectionText>
+        I am an open source developer and student at Rwanda Coding Academy. I am
+        available to work on any project which you wish me to accompany with
+        you.
+      </SectionText>
+      <Button onClick={() => (window.location = "#about")}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;

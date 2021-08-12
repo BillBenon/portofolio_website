1047a633a8d85cbc3bff1a239758dabdffec1880
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-12 23:41:20 +0200

    finished the technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -17,11 +17,44 @@ import {
 const Technologies = () => (
   <Section id="tech">
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range of technologies in web development world. From
-      Backend To Frontend
+      Backend To Design
     </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Frontend</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            ReactJS, AngularJS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Backend</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            NodeJS and DataBases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            tools like Figma, XD, Photoshop
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
